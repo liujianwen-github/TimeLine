bae96b80e8e49a0452883c35691187c032ef5c3a
insight <747127676@qq.com>
2018-02-24 08:07:41 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,186 @@
+(function($){
+	function showImgs(){
+		console.log($(this).parents('.mainImages'))
+	}
+	$('#imagesStream').initImagesStream([{
+		source:"img/dog.jpg",
+		style:{
+			position:'top',
+			size:50
+		}
+	},{
+		source:"img/dog.jpg",
+		style:{
+			position:'middle',
+			size:30
+		}
+	},{
+		source:"img/dog.jpg",
+		style:{
+			position:'bottom',
+			size:20
+		}
+	},{
+		source:"img/dog.jpg",
+		style:{
+			position:'top',
+			size:100
+		}
+	},{
+		source:"img/dog.jpg",
+		style:{
+			position:'middle',
+			size:10
+		}
+	},{
+		source:"img/dog.jpg",
+		style:{
+			position:'bottom',
+			size:20
+		}
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	},{
+		source:"img/dog.jpg"
+	}])
+//	$('#imagesStream .box').on('click',function(){
+//		alert('1')
+//	})
+	var imgs = $('#imagesStream .box')
+	$('body').on('click',"#imagesStream .box",function(){
+		alert('1')
+	})
+	console.log(imgs)
+//	imgs.each(function(inedx,el){
+//		console.log(el)
+//		initHammer({
+//			dom:el
+//		})
+//	})
+	var basicPosition = []
+	imgs.each(function(index,el){
+		console.log(el)
+		initHammer({
+			dom:el
+		})
+	})
+	imgStream(60000)
+	function imgStream(speed){
+		$('#imagesStream').animate({'left':'-500vw'},speed,"linear",function(){
+			$('#imagesStream').animate({'left':'0'},speed,"linear",function(){
+				imgStream(speed)
+			})
+		})
+	}
+	
+//	var i = new Hammer()
+	console.log(Hammer)
+	$('.showBox').initTimeLine([{
+		name:'1937',
+		mainImg:"img/dog.jpg"
+	},{
+		name:'1938',
+		mainImg:"img/dog.jpg"
+	},{
+		name:'1939',
+		mainImg:"img/dog.jpg"
+	},{
+		name:'1940',
+		mainImg:"img/dog.jpg"
+	},{
+		name:'1941',
+		mainImg:"img/dog.jpg"
+	},{
+		name:'1942',
+		mainImg:"img/dog.jpg"
+	}])
+	$('body').on('touchstart','.item .footer img',function(e){
+		var _this = this
+		console.log(this)
+		if($(this).parents('.item').hasClass('active')){
+			$(this).find('.item').removeClass('active')
+			$(this).parents('.item').find('.mainImages').animate({height:0},500)
+			$(this).attr('src','img/point.png')
+			$(this).parents('.item').removeClass('active')
+			return
+		}
+		$(this).attr('src','img/select.png')
+		$(this).parents('.item').addClass('active')
+		$(this).parents('.footer').siblings('.mainImages').animate({height:'50vh'},500)
+//		$(this).parents('.item').siblings().find('.mainImages').animate({height:0},500)
+//		$(this).parents('.item').siblings().find('.footer').find('img').attr('src','img/point.png')
+	})
+//	return
+	console.log('init')
+	$('#clear').on('click',function(){
+		$('#log').html('')
+	})
+
+	
+	function initHammer (args){
+		console.log(args)
+		var dom = args.dom
+		var options = {}
+		var handlePanStart = function(){
+	//		确定初始位置
+			var left = $(dom).css('left')
+			var l = left.search('px')
+			left = Number(left.substring(0,l))
+			var top = $(dom).css('top')
+			var t = top.search('px')
+			top = Number(top.substring(0,t))
+			basicPosition = [{
+				left:left,
+				top:top
+			}]
+		}
+		var handlePan = function(e){
+			console.log(e)
+	//		$('#log').log('pan')
+			$(dom).css({'left':basicPosition[0].left+e.deltaX+'px','top':basicPosition[0].top+e.deltaY+'px'})
+		}
+		var handlePinch = function(e){
+	//		console.log(e)
+			if(e.type == "pinchstart"){
+				$('#log').log('pinchstart')
+				//TODO 注意  transform只能有一条属性
+				var transform = dom.style.transform
+				transform = transform.replace(/\(/,"")
+				transform = transform.replace(/\)/,"")
+				transform = transform.substring(5)
+				$('#log').log(transform)
+	         	basicPosition[0].scaleCount = transform || 1
+	//       	$('#log').log(basicPosition[0].scaleCount)
+	       	}
+			console.log(dom.style.transform.scale)
+			$('#log').log(basicPosition[0].scaleCount)
+	        $(dom).css('transform','scale('+basicPosition[0].scaleCount * e.scale+')');
+	//      $(dom).css('transform',"scale(" + (e.scale)+ ")")
+		}
+		var mc = new Hammer(dom, options);
+		mc.get('pinch').set({enable:true})
+	//	mc.get('swipe').set({})
+	//	mc.add( new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0 }) );
+	//	mc.add( new Hammer.Pinch({direction: Hammer.DIRECTION_ALL}));
+		mc.on('panstart',handlePanStart);
+		mc.on("pan", handlePan);
+		mc.on('pinchstart pinch',handlePinch);
+	}
+
+//	var dom = document.getElementsByClassName('curr')[0]
+	
+})(jQuery)
---
+++ js/util.js
@@ -0,0 +1,80 @@
+(function($){
+	/**
+	 * 初始化时间轴
+	 * @param {Object} arg
+	 */
+	$.fn.initTimeLine = function(args){
+		var _this = this
+		args.forEach(function(e,i,arr){
+			var str = `<div class="item">`+
+						`<div class="mainImages">`+
+							`<div class="curr" style="transform: scale(1);">`+
+								`<img src=${e.mainImg}/>`+
+							`</div>`+
+							`<div class="line"></div>`+
+						`</div>`+
+						`<div class="footer">`+
+							`<img src="img/point.png"/>`+
+							`<p>${e.name}</p>`+
+						`</div>`+
+					`</div>`
+			$(_this).append(str)	
+		})
+	}
+	/**
+	 * 初始化图片流
+	 * @param {Object} args
+	 */
+	$.fn.initImagesStream = function(args){
+		if(args.length===0||typeof args ==='undefined'){
+			console.error('参数错误!')
+			return
+		}
+		var _this = this
+		var arrH = []
+		args.forEach(function(el,index,arr){
+			console.log(el)
+			var img = new Image()
+			img.src = el.source
+			
+			var box = $('<div class="box"></div>')
+			var pic = $('<div class="pic"></div>')
+			pic.append(img)
+			box.append(pic)
+			console.log(parseInt(Math.random()*20))
+			console.log(el.style)
+			if(el.style){
+				console.log(el.style)
+				img.style.width = el.style.size+ 'vh'
+				switch (el.style.position){
+					case"top":{
+						$(box).css('vertical-align','top')
+						break;
+					}
+					case"middle":{
+						$(box).css('vertical-align','middle')
+						break;
+					}
+					case"bottom":{
+						$(box).css('vertical-align','bottom')
+						break;
+					}
+				}
+			}else{
+				img.style.width = parseInt(Math.random()*20)+50 + 'vh'
+			}
+			$(_this).append(box)
+		})
+	}
+	/**
+	 * 移动端打印日志
+	 * @param {Object} 日志内容
+	 */
+	$.fn.log = function(e){
+		var span = $('<span>'+e+';</span>')
+		$(this).append(span)
+		return function(){
+			$(this).html('')
+		}
+	}
+})(jQuery)
